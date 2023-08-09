07249f41b0f39928199b5b45a1231e1cd8e8e3ad
Felipe Araujo <araujo.felipedev@gmail.com>
2023-08-09 08:06:57 -0300

    adding dark mode

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,6 +1,9 @@
 <template>
 <header>
    <h1><img src="../assets/logoAluraTracker.png" alt=""></h1>
+   <button class="button" @click="alterarTema">
+    {{  textoBotao }}
+   </button>
 </header>
 </template>
 
@@ -9,6 +12,26 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
    name: 'BarraLateral',
+   emits: ['aoTemaAlterado'],
+   data() {
+    return {
+      modoEscuroAtivo: false
+    }
+   },
+   computed: {
+    textoBotao() {
+      if(this.modoEscuroAtivo) {
+        return 'Desativar modo escuro'
+      }
+      return 'Ativar modo escuro'
+    }
+   },
+   methods: {
+    alterarTema() {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo
+      this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
+    }
+   },
 })
 
 </script>
@@ -19,6 +42,7 @@ header {
   background: #0d3b66;
   width: 100%;
   height: 100vh;
+  text-align: center;
 }
 @media only screen and (max-width: 768px) {
   header {
